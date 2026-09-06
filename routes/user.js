4e0695268409d6adff5d6f625eff0392fad643a9
SanyaJaiswal01 <sanyajais01@gmail.com>
2026-09-07 02:44:32 +0530

    Fix import case for ExpressError module

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express= require("express");
 const router =express.Router();
 
 const wrapAsync=require("../utils/wrapAsync.js");
-const ExpressError=require("../utils/expressError.js");
+const ExpressError=require("../utils/ExpressError.js");
 const passport = require("passport");
 
 const User=require("../models/user.js");
